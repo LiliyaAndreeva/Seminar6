71d1421e40da5bcf637aa09080f0d81d7de7b667
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2023-07-28 15:37:34 +0500

    Задание по семинару 6

---
+++ src/main/java/Sem6.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Sem6 {
+    public static void main(String[] args) {
+        MyHashSet prim = new MyHashSet();
+        prim.add(1);
+        System.out.println(prim.add(5));
+        HashSet<Integer> nameSet = new HashSet<>();
+    }
+}
+
+class MyHashSet {
+    private HashMap<Integer, Object> myDB = new HashMap<>();
+    private static final Object MYOBJ = new Object();
+    public boolean add(int elem){
+        return myDB.put(elem, MYOBJ)==null;
+    }
+
+    public boolean remove(int elem){
+        return myDB.remove(elem) == MYOBJ;
+    }
+
+    public boolean isEmpty(){
+        return myDB.size()==0;
+    }
+
+    public int findKey(int id){
+        Object[] a = myDB.keySet().toArray();
+        return (int) a[id];
+    }
+}
+
